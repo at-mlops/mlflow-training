3adad19766be223b0ac41f2c47128d146dcaf252
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2023-07-17 16:59:34 +0200

    add keras and skicit final projects

---
+++ 05-Final-project/README-05-final-project.md
@@ -0,0 +1,15 @@
+# Final Project
+
+There are two Projects to choose one. Only one is needed to pass the final exercise. Yet, of course you can do both if you are keen on doing so.
+
+Extend the code with everything you know about MLflow now, meaning you track relevant metrics, parameters, further model information & artifacts, and register the model to the Mlflow Registry.
+You can also choose to change the code as needed, meaning to calculare further metrics or intend blocks of code. It's up to you. The goal of this exercise is that you make yourself familiar with the MLflow API on a real ML example! Of course, you can choose `mlflow.autolog()` if you are lame and don't want to learn anything. ;)
+
+## Keras | CNN
+
+The first example implements a basic Keras Convolutional Neural Network on the MNIST dataset. The example can also be found on the official [keras documentation](https://keras.io/examples/vision/mnist_convnet/).
+
+## Scikit | SVM
+
+The second example implements a scikit based Support Vector Machine based on the breast cancer dataset. Its based on a [datacamp tutorial](https://www.datacamp.com/tutorial/svm-classification-scikit-learn-python), so if you are itnerested in it you can check it out of course.
+
---
+++ 05-Final-project/scikit/exercise-05-final-project-svm-cancer.py
@@ -0,0 +1,43 @@
+# Import scikit-learn dataset library
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn import svm
+
+# Load dataset
+cancer = datasets.load_breast_cancer()
+
+print("Features: ", cancer.feature_names)
+print("Labels: ", cancer.target_names)
+
+# print data(feature)shape
+print("Shape: ", cancer.data.shape)
+
+# print the cancer data features (top 5 records)
+print("cancer.data[0:5]: ", cancer.data[0:5])
+
+# print the cancer labels (0:malignant, 1:benign)
+print("cancer.target: ",cancer.target)
+
+## Model training
+
+# Split dataset into training set and test set; 70% training and 30% test
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.3,random_state=109) 
+
+#Create a svm Classifier
+clf = svm.SVC(kernel='linear') # Linear Kernel
+
+#Train the model using the training sets
+clf.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+# Model Accuracy: how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+# Model Precision: what percentage of positive tuples are labeled as such?
+print("Precision:",metrics.precision_score(y_test, y_pred))
+
+# Model Recall: what percentage of positive tuples are labelled as such?
+print("Recall:",metrics.recall_score(y_test, y_pred))
