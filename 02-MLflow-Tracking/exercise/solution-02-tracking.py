e999d1b6ddf3ac898520922e09aaf5baa4b9c789
Jahed Naghipoor <jahed.naghipoor@alexanderthamm.com>
2023-08-07 12:03:26 +0200

    some bugs in solution fixed

--- 02-MLflow-Tracking/exercise/solution-02-tracking.py
+++ 02-MLflow-Tracking/exercise/solution-02-tracking.py
@@ -3,44 +3,50 @@ import os
 # TODO: import mlflow
 import mlflow
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
 
-# -- Params
-# TODO log a learning_rate of 0.01
-mlflow.log_param("learning_rate", 0.01)
+experiment_name = "MLflow-training-YOUR-NAME"
+mlflow.set_experiment(experiment_name)
 
-params = {"epochs": 0.05, "final_activation": "sigmoid"}
-# TODO: log the above mentioned parameters to mlflow
-mlflow.log_params(params)
-
-# -- Tags
-# TODO: set an environment tag to dev and a username tag to your name
-mlflow.set_tags({"environment": "dev", "username": "sebastian-blum"})
-
-# -- Metrics
-# TODO: Log a F-score of 0.7
-mlflow.log_metric("F-score", 0.7)
+run_name = "logistic-regression"
 
 # TODO: log the following accuracies as metrics to your logistic-regression run
-lr_run_id = "488529c8b16a4f7784aeb3f32e3f63d9"
+
 accuracy_list = [0.6, 0.6, 0.8, 0.9]
-with mlflow.start_run(run_id=lr_run_id):
+with mlflow.start_run(run_name=run_name):
+
+    # -- Params
+    # TODO log a learning_rate of 0.01
+    mlflow.log_param("learning_rate", 0.01)
+
+    params = {"epochs": 0.05, "final_activation": "sigmoid"}
+    # TODO: log the above mentioned parameters to mlflow
+    mlflow.log_params(params)
+
+    # -- Tags
+    # TODO: set an environment tag to dev and a username tag to your name
+    mlflow.set_tags({"environment": "dev", "username": "sebastian-blum"})
+
+    # -- Metrics
+    # TODO: Log a F-score of 0.7
+    mlflow.log_metric("F-score", 0.7)
     for val_acc in accuracy_list:
         mlflow.log_metric("val_acc", val_acc)
 
-# -- Artifacts
-# Create an example file output/test.txt
-file_path = "outputs/test.txt"
-if not os.path.exists("outputs"):
-    os.makedirs("outputs")
-with open(file_path, "w") as f:
-    f.write("hello world!")
+    # -- Artifacts
+    # Create an example file output/test.txt
+    file_path = "outputs/test.txt"
+    if not os.path.exists("outputs"):
+        os.makedirs("outputs")
+    with open(file_path, "w") as f:
+        f.write("hello world!")
 
-# TODO: store the previous created file in the data/subfolder subdirectory
-mlflow.log_artifact(local_path=file_path, artifact_path="data/subfolder")
+    # TODO: store the previous created file in the data/subfolder subdirectory
+    mlflow.log_artifact(local_path=file_path, artifact_path="data/text")
 
-# TODO: get and print the URI where the artifacts have been logged to
-artifact_uri = mlflow.get_artifact_uri()
-print(artifact_uri)
+    # TODO: get and print the URI where the artifacts have been logged to
+    artifact_uri = mlflow.get_artifact_uri()
+    print(artifact_uri)
 
 
 # -- Autolog
